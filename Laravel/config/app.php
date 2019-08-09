7995731fe58533d297cb8dc058cf89fbf6a0a52f
Carrasco <fernando.carrasco0@gmail.com>
2019-08-09 10:40:28 -0300

    + Adicionado a Macro para repostas Json

--- Laravel/config/app.php
+++ Laravel/config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Provider para padronizacao de respostas em json
+        App\Providers\ResponseJsonProvider::class
+
+
     ],
 
     /*
