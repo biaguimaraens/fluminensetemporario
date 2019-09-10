945b4f1e6808a63b3a93807f594c1f16ccbe49a6
Danilo Collares de Castro <danilo@ejcm.com.br>
2019-09-10 11:56:19 -0300

    * modificações do calendário para adicionar eventos

--- Angular/src/app/componentes/calendario/calendario.component.ts
+++ Angular/src/app/componentes/calendario/calendario.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
 
 @Component({
   selector: 'app-calendario',
@@ -8,9 +12,35 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class CalendarioComponent implements OnInit {
 
-  calendarPlugins = [dayGridPlugin];
-  constructor(){}
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent; // the #calendar in the template
 
-  ngOnInit(){}
+  calendarVisible = true;
+  calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
+  calendarWeekends = true;
+  calendarEvents: EventInput[] = [
+    { title: 'Event Now', start: new Date() }
+  ];
 
+  toggleVisible() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  toggleWeekends() {
+    this.calendarWeekends = !this.calendarWeekends;
+  }
+
+  gotoPast() {
+    let calendarApi = this.calendarComponent.getApi();
+    calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
+  }
+
+  handleDateClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    }
+  }
 }
