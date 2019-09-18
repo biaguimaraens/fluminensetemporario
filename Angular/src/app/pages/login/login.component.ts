60198374490a1f76221a0aa93a20318a022e2400
RuasGAR <gabrielamaralruas@gmail.com>
2019-09-17 22:43:01 -0300

    +Rota para atividades junto à função de login

--- Angular/src/app/pages/login/login.component.ts
+++ Angular/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
 
   login: FormGroup
-  constructor(public builder: FormBuilder, public auth: AuthService) {
+  constructor(public builder: FormBuilder, public auth: AuthService, private router: Router) {
     this.login = this.builder.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required, Validators.minLength(6)]]
@@ -21,9 +22,13 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(login) {
-    console.log(login.value)
-    this.auth.login(login.value).subscribe((res) => {
-      console.log(res);
-    });
+    
+    if (login.status == "VALID") {
+      console.log(login.value)
+      this.auth.login(login.value).subscribe((res) => {
+        console.log(res);
+      });
+      this.router.navigate(['/atividade']);
+    }
   }
 }
